419305c8d22b19adef5c3404604a6afd2e384652
Abishek Kumar <111441641+Abishekkumark@users.noreply.github.com>
2023-12-03 00:20:34 +0530

    Update

---
+++ square_rectangle.sol
@@ -0,0 +1,12 @@
+//SPDX-LICENSE-IDENTIFIER:LICENSED
+pragma solidity 0.8.0;
+
+function square(uint a) public pure returns(uint)
+{
+return a*a;
+}
+
+function rectangle(uint a ,uint b) public pure returns(uint)
+{
+return a*b;
+}
